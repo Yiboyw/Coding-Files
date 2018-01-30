9e7df727783c278588718aa977dd72bdc9460560
Yibo Wang <yibowangyw@gmail.com>
2018-01-29 20:38:18 -0500

    Update SmallestInteger.java

--- SmallestInteger.java
+++ SmallestInteger.java
@@ -5,9 +5,7 @@ Write a function that, given an array A of N integers, returns the smallest posi
 that does not occur in A.
 
 For example, given A = [1, 3, 6, 4, 1, 2], the function should return 5.
-
 Given A = [1, 2, 3], the function should return 4.
-
 Given A = [−1, −3], the function should return 1.
 
 Assume that:
